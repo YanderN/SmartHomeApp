b03cb6b9d7950dad03c295a6ca2efe20c05f630f
Yander <48702338+YanderN@users.noreply.github.com>
2022-03-18 18:20:50 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthomeapp/Config.java
@@ -0,0 +1,5 @@
+package mx.tecnm.cdhidalgo.smarthomeapp;
+
+public class Config {
+    public static  final String url = "http://192.168.1.102/smarthome/smarthome/";
+}
---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthomeapp/RecyclerViewOnItemClickListener.java
@@ -0,0 +1,9 @@
+package mx.tecnm.cdhidalgo.smarthomeapp;
+
+import android.view.View;
+
+public interface RecyclerViewOnItemClickListener {
+    void onClick(View v, int position);
+    void onClickEdit(View v, int position);
+    void onClickDel(View v, int position);
+}
